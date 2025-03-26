1a4504c44504986b037a87a609afc02c5e651abc
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-26 13:51:34 +0530

    feat: created initial application

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,15 @@ class App {
   constructor() {
     this.app = express();
   }
+  private setMiddleWares(): void {
+    this.app.use(express.json({ limit: '16kb' }));
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.static('public'));
+    this.app.set('view engine', 'ejs');
+  }
+
+  private setRoutes(): void {}
+
   public getServer(): Application {
     return this.app;
   }

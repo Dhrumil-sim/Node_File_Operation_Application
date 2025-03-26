088730d25261ac409bc253b5e8cf226d551034cd
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-26 13:52:01 +0530

    feat: configured multer for file upload

---
+++ src/configurations/multerConfiguration.ts
@@ -0,0 +1,18 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 10 * 1024 * 1024 },
+}).single('file');
+
+module.exports = upload;

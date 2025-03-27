586b946d4feb866bd809079b8b9571c6b6932f4e
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-03-27 09:50:01 +0530

    fix: file will be uploaded with original name

--- src/configurations/multer.configuration..ts
+++ src/configurations/multer.configuration..ts
@@ -1,12 +1,10 @@
 import multer from 'multer';
-import path from 'path';
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './public/uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, file.originalname);
   },
 });
 
